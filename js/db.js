053a6fbd4f2b3782252b0ca8b8a73b565b7d9235
jorgemr86 <jorgevallejo9@yahoo.es>
2023-07-27 14:52:55 -0500

    package.json

---
+++ js/db.js
@@ -0,0 +1,60 @@
+productos = [
+    {
+      id: 1,
+      nombre: "Tanga",
+      precio: 150000,
+      img:
+        "assets/img/41Pin-removebg-preview.png",
+      cantidad: 1,
+      info: "Braguitas suaves de encaje elástico, perfectas para curvas y máxima comodidad.Una variedad de colores sólidos y bellas imágenes femeninas crean una apariencia fresca y sexy.Correas ultra lisas que no ruedan y etiquetas de satén para la comodidad durante todo el día. " , 
+     
+      
+    },
+    {
+      id: 2,
+      nombre: "Tanga",
+      precio: 100000,
+      img:
+        "assets/img/tanga-removebg-preview.png",
+      cantidad: 1,
+      info:"Tangas en microfibra acanalada con tiro medio y refuerzo interior. Un complemento esencial para tu armario.<br> Composición <br>Elastano 10%,Poliamida 90%"
+    },
+    {
+      id: 3,
+      nombre: "Tanga",
+      precio: 150000,
+      img:
+      "assets/img/tanga3-removebg-preview.png",
+    cantidad: 1,
+    info: " Tanga sin costuras con cintura elástica. No tiene costuras que molesten y es por lo tanto muy cómodo. Producto hecho con un 90% de poliamida y un 10% de elastán. Gracias a la adición de elastán, la tela es más flexible y elástica. Este producto no contiene sustancias nocivas y lleva la certificación de calidad de Oeko-Tex." , 
+   
+    },
+    {
+      id: 4,
+      nombre: "Braga",
+      precio: 200000,
+      img:
+        "assets/img/bragas-removebg-preview.png",
+      cantidad: 1,
+      info:" Tejido casi desnudo ultraligero, ultrasuave y resistente a la humedad"
+    },
+    {
+      id: 5,
+      nombre: "Braga",
+      precio: 200000,
+      img:
+        "assets/img/bragas2-removebg-preview.png",
+      cantidad: 1,
+      info:"  ultrasuave y resistente a la humedad Material 76% POLIAMIDA/POLYAMIDE <br> 24% ELASTANO/ELASTANE <br>REFUERZO/GUSSET LINING: 100% ALGODÓN." , 
+    },
+    {
+      id: 6,
+      nombre: "Mini Tanga",
+      precio: 150000,
+      img:
+        "assets/img/mini-t-removebg-preview.png",
+      cantidad: 1,
+      info:"  MINI TANGA REVERSIBLE LEOPARDO LILA NEON <br>" , 
+    },
+  ];
+  
