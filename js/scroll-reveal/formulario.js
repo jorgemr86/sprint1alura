e226f3a25684c9ef5c8d21480a2c4090f97329fd
jorgemr86 <jorgevallejo9@yahoo.es>
2023-08-04 21:48:53 -0500

    contacto-lista

--- js/scroll-reveal/formulario.js
+++ js/scroll-reveal/formulario.js
@@ -1,4 +1,4 @@
-document.querySelector('#contacto-form').addEventListener('submit', (e) => {
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
     e.target.elements.name.value = '';
     e.target.elements.email.value = '';
